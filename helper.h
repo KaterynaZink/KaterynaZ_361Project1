52bc6bdb8b47a8431eb15ef656006ec4abd38d5c
KaterynaZink <katzink@udel.edu>
2026-09-12 16:21:07 -0400

    fixed prototypes

--- helper.h
+++ helper.h
@@ -26,16 +26,9 @@ typedef struct List{
 void initialize_list(List *list);
 void add_student(List *list, Student student);
 void remove_student(List *list, Student student);
-void print_beginning(List *list);
-void print_end(List *list);
+void print_beginning(const List *list);
+void print_end(const List *list);
 void free_list(List *list);
-
-
-
-
-
-
-
 
 
 #endif //helper_h
