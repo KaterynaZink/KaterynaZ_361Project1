d0a9703e63031cc1a5376f422bddc09293d1823d
KaterynaZink <katzink@udel.edu>
2026-09-12 16:27:14 -0400

    added prototypes to helper.c

---
+++ helper.c
@@ -0,0 +1,20 @@
+#include "helper.h"
+
+void initialize_list(List *list){
+
+}
+void add_student(List *list, Student student){
+
+}
+void remove_student(List *list, Student student){
+
+}
+void print_beginning(const List *list){
+
+}
+void print_end(const List *list){
+
+}
+void free_list(List *list){
+    
+}
